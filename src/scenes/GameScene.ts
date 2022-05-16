0bd7b092a80aa030e7a963e9469e931e76fa9bed
sourcepirate <sathyanarrayanan@yandex.com>
2022-05-16 15:23:03 +0800

    camera and player rendering

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,13 +1,17 @@
-import { Scene, Math } from "phaser";
+import { Scene, Math, Physics } from "phaser";
 
 
 export default class GameScene extends Scene {
+
+    private player?: Physics.Arcade.Sprite ;
+    private platforms?: Physics.Arcade.StaticGroup;
+
     constructor() {
         super('GameScene');
     }
 
     preload() {
-        this.load.image('bg_layer_1', 'assets/PNG/Background/bg_layer1.png');
+        this.load.image('bg_layer_1', 'assets/PNG/Background/bg_layer1.png').setScrollFactor(1, 0);
         this.load.image('grass', 'assets/PNG/Environment/ground_grass.png');
         // -- initalizing player
 
@@ -17,19 +21,46 @@ export default class GameScene extends Scene {
     create() {
         this.add.image(240, 320, 'bg_layer_1');
         // this.physics.add.image(240, 320, 'grass').setScale(0.5);
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
 
         for (let i = 0; i <= 5; i++) {
             const x = Math.Between(80, 380);
             const y = 150 * i;
 
-            const platform = platforms.create(x, y, 'grass');
+            const platform = this.platforms.create(x, y, 'grass');
             platform.setScale(0.5);
 
             platform.body.updateFromGameObject();
         }
 
-        const player = this.physics.add.sprite(240, 320, 'bunny_stand').setScale(0.5);
-        this.physics.add.collider(platforms, player);
+        this.player = this.physics.add.sprite(240, 320, 'bunny_stand').setScale(0.5);
+
+        this.player.body.checkCollision.up = false;
+        this.player.body.checkCollision.left = false;
+        this.player.body.checkCollision.right = false;
+
+        this.physics.add.collider(this.platforms, this.player);
+
+        this.cameras.main.startFollow(this.player);
+    }
+
+    update(): void {
+
+        this.platforms?.children.iterate(child => {
+            const platform = child;
+            const scrollY = this.cameras.main.scrollY;
+            if(platform.y >= scrollY + 700) {
+                platform.y = scrollY - Math.Between(50, 100);
+                platform.body.updateFromGameObject();
+            }
+        })
+
+
+        const isPlayerTouchingDown = this.player?.body.touching.down;
+        if(isPlayerTouchingDown) {
+            this.player?.setVelocityY(-300);
+        }
+
+
     }
 }
